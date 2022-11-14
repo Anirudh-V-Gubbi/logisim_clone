711a916313ca6341301ae2a0b32ba962348e6292
Anirudh V Gubbi <anirudh@Anirudhs-MacBook-Pro.local>
2022-11-14 23:41:17 +0530

    initial - basic window renderer

---
+++ vendors/glew/2.2.0_1/.brew/glew.rb
@@ -0,0 +1,80 @@
+class Glew < Formula
+  desc "OpenGL Extension Wrangler Library"
+  homepage "https://glew.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/glew/glew/2.2.0/glew-2.2.0.tgz"
+  sha256 "d4fc82893cfb00109578d0a1a2337fb8ca335b3ceccf97b97e5cc7f08e4353e1"
+  license "BSD-3-Clause"
+  revision 1
+  head "https://github.com/nigels-com/glew.git", branch: "master"
+
+  depends_on "cmake" => [:build, :test]
+
+  on_linux do
+    depends_on "freeglut" => :test
+    depends_on "mesa-glu"
+  end
+
+  def install
+    system "cmake", "-S", "./build/cmake", "-B", "_build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
+    doc.install Dir["doc/*"]
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~EOS
+      project(test_glew)
+
+      set(CMAKE_CXX_STANDARD 11)
+
+      find_package(OpenGL REQUIRED)
+      find_package(GLEW REQUIRED)
+
+      add_executable(${PROJECT_NAME} main.cpp)
+      target_link_libraries(${PROJECT_NAME} PUBLIC OpenGL::GL GLEW::GLEW)
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <GL/glew.h>
+
+      int main()
+      {
+        return 0;
+      }
+    EOS
+
+    system "cmake", ".", "-Wno-dev"
+    system "make"
+
+    glut = if OS.mac?
+      "GLUT"
+    else
+      "GL"
+    end
+    (testpath/"test.c").write <<~EOS
+      #include <GL/glew.h>
+      #include <#{glut}/glut.h>
+
+      int main(int argc, char** argv) {
+        glutInit(&argc, argv);
+        glutCreateWindow("GLEW Test");
+        GLenum err = glewInit();
+        if (GLEW_OK != err) {
+          return 1;
+        }
+        return 0;
+      }
+    EOS
+    flags = %W[-L#{lib} -lGLEW]
+    if OS.mac?
+      flags << "-framework" << "GLUT"
+    else
+      flags << "-lglut"
+    end
+    system ENV.cc, testpath/"test.c", "-o", "test", *flags
+    # Fails in Linux CI with: freeglut (./test): failed to open display ''
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    system "./test"
+  end
+end
---
+++ vendors/glfw/3.3.8/.brew/glfw.rb
@@ -0,0 +1,49 @@
+class Glfw < Formula
+  desc "Multi-platform library for OpenGL applications"
+  homepage "https://www.glfw.org/"
+  url "https://github.com/glfw/glfw/archive/3.3.8.tar.gz"
+  sha256 "f30f42e05f11e5fc62483e513b0488d5bceeab7d9c5da0ffe2252ad81816c713"
+  license "Zlib"
+  head "https://github.com/glfw/glfw.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  on_linux do
+    depends_on "freeglut"
+    depends_on "libxcursor"
+    depends_on "mesa"
+  end
+
+  def install
+    args = std_cmake_args + %w[
+      -DGLFW_USE_CHDIR=TRUE
+      -DGLFW_USE_MENUBAR=TRUE
+      -DBUILD_SHARED_LIBS=TRUE
+    ]
+
+    system "cmake", *args, "."
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #define GLFW_INCLUDE_GLU
+      #include <GLFW/glfw3.h>
+      #include <stdlib.h>
+      int main()
+      {
+        if (!glfwInit())
+          exit(EXIT_FAILURE);
+        glfwTerminate();
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-o", "test",
+                   "-I#{include}", "-L#{lib}", "-lglfw"
+
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    system "./test"
+  end
+end
