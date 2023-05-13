e09489a8fab9397cf88a4e3d3484e36c5ee6a6d4
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-05-13 14:05:08 +0530

    included socket state as an argument for the callback and added a API to change socket state

--- Logisim(clone)/Entities/sockets/socket.h
+++ Logisim(clone)/Entities/sockets/socket.h
@@ -22,13 +22,13 @@ public:
     Socket(glm::ivec2 position, glm::vec2 absPosition, SocketState state) : m_position{position}, m_absPosition{absPosition}, m_state{state} {
         m_color = socketStateColorMap.at(m_state);
     }
-    Socket(glm::ivec2 position, glm::vec2 absPosition, SocketState state, std::function<void()>* entityCallback) : m_position{position}, m_absPosition{absPosition}, m_state{state}, m_entityCallback{entityCallback} {
+    Socket(glm::ivec2 position, glm::vec2 absPosition, SocketState state, std::function<void(SocketState)>* entityCallback) : m_position{position}, m_absPosition{absPosition}, m_state{state}, m_entityCallback{entityCallback} {
         m_color = socketStateColorMap.at(m_state);
     }
     Socket(glm::ivec2 position, glm::vec2 absPosition) : m_position{position}, m_absPosition{absPosition},  m_state{SocketState::UNINITIALIZED} {
         m_color = socketStateColorMap.at(m_state);
     }
-    Socket(glm::ivec2 position, glm::vec2 absPosition, std::function<void()>* entityCallback) : m_position{position}, m_absPosition{absPosition},  m_state{SocketState::UNINITIALIZED}, m_entityCallback{entityCallback} {
+    Socket(glm::ivec2 position, glm::vec2 absPosition, std::function<void(SocketState)>* entityCallback) : m_position{position}, m_absPosition{absPosition},  m_state{SocketState::UNINITIALIZED}, m_entityCallback{entityCallback} {
         m_color = socketStateColorMap.at(m_state);
     }
     Socket() : m_position{glm::ivec2(0, 0)}, m_state{SocketState::UNINITIALIZED} {
@@ -48,12 +48,19 @@ public:
         return this->m_color;
     }
     
+    void ChangeState(SocketState newState) {
+        if(m_state != newState) {
+            m_state = newState;
+            if(m_entityCallback) (*m_entityCallback)(newState);
+        }
+    }
+    
 private:
     glm::ivec2 m_position;
     glm::vec2 m_absPosition;
     SocketState m_state;
     glm::vec3 m_color;
-    std::function<void()>* m_entityCallback;
+    std::function<void(SocketState)>* m_entityCallback;
 };
 
 #endif
